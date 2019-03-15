047990c8a25bf15c657eb94fdafd3c09ae35747f
Dogeek <Dogeek@users.noreply.github.com>
2019-03-15 22:13:36 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+try:
+    import tkinter as tk
+    import tkinter.ttk as ttk
+    import tkinter.filedialog as filedialog
+except ImportError:
+    import Tkinter as tk
+    import ttk
+    import Tkinter.tkFileDialog as filedialog
+
+from copy import copy
